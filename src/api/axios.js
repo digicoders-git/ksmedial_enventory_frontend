4f669dd4f4a69f60d1bb0435c9d10fc00c95c3e8
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-01-29 21:28:24 +0530

    url setup done

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
     withCredentials: true,
 });
+
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('ks_shop_token');
