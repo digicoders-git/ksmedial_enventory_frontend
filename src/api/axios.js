f71e65b4fd57d2a843f9d2d5966865ee195b785d
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-01-29 21:35:36 +0530

    done

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
-    withCredentials: true,
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api'
 });
+
 
 
 api.interceptors.request.use((config) => {
